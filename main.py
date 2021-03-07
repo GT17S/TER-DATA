b672f7aefbeecc9e9203cbfafda85e1c568da371
Nadjibian <45125991+Nadjibian@users.noreply.github.com>
2021-03-07 18:00:47 +0100

    affichage des graph sur une figure

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cmath import sin
+
 import mplcursors as mplcursors
 import psycopg2
 import pandas as pd
@@ -26,22 +28,30 @@ from gmi
 group by year 
 order by year 
 ''',connection)
+fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,10))
+ax1.plot(test1['year'],test1['personal'])
+ax1.set_title("l'évolution de personal depuis 1990")
+ax1.set_ylabel("nombres de personal")
+ax2.bar(test['year'],test['moyenne'],color='red')
+ax2.set_title("l'évolution d'homicide depuis 2000")
+ax2.set_ylabel("la moyenne d'homicide par années")
+plt.show()
 
+'''
 test.plot(kind="bar",x="year",y="moyenne",color='red')
 
 plt.ylabel("la moyenne d'homicide par années")
 plt.title("l'évolution d'homicide depuis 2000")
 
-test1.plot(kind="line",x="year",y="personal")
+#test1.plot(kind="line",x="year",y="personal")
 plt.ylabel("nombres de personal")
 plt.title("l'évolution de personal depuis 1990")
 mplcursors.cursor(hover=True)
 
-
 plt.show()
 
 
-
+'''
 
 '''df = pd.DataFrame.from_dict(test, orient='index', dtype=None)'''
 '''df.head()'''
