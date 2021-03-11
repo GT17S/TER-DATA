c4055eaf6b511ffcf91bdf0c6348a646d1e2705e
Nadjibian <45125991+Nadjibian@users.noreply.github.com>
2021-03-11 15:09:06 +0100

    test

--- Database/conxion.py
+++ Database/conxion.py
@@ -3,4 +3,4 @@ cnx = psycopg2.connect(database="TER",
                               user="postgres",
                               password="halliche",
                               host="localhost",
-                            port=5432)
+    port=5432)
