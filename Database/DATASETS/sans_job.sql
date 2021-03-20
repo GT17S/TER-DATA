306e7107e80a3eef490575449ffbd89ba17e782b
Nadjibian <45125991+Nadjibian@users.noreply.github.com>
2021-03-20 17:36:32 +0100

    ajout d'une table pourcentage des non employés

---
+++ Database/DATASETS/sans_job.sql
@@ -0,0 +1,52 @@
+CREATE TABLE sjr_import (
+	country text NOT NULL,
+    year integer NOT NULL,
+    value numeric
+);
+
+copy sjr_import from 'sans_job_import.csv' WITH DELIMITER ';' CSV HEADER;
+SELECT DISTINCT country
+  FROM sjr_import
+ WHERE country NOT IN (SELECT nicename FROM countries);
+
+UPDATE sjr_import SET country = 'United Arab Emirates' WHERE country = 'UAE';
+UPDATE sjr_import SET country = 'Saint Lucia' WHERE country = 'St. Lucia';
+
+UPDATE sjr_import SET country = 'Sao Tome and Principe' WHERE country = 'Sao Tome And Principe';
+UPDATE sjr_import SET country = 'Cape Verde' WHERE country = 'Cabo Verde';
+
+UPDATE sjr_import SET country = 'Brunei Darussalam' WHERE country = 'Brunei';
+UPDATE sjr_import SET country = 'Korea, Democratic People''s Republic of' WHERE country = 'North Korea';
+
+UPDATE sjr_import SET country = 'Congo, the Democratic Republic of the' WHERE country = 'Congo, Rep.';
+UPDATE sjr_import SET country = 'Libyan Arab Jamahiriya' WHERE country = 'Libya';
+
+UPDATE sjr_import SET country = 'Tanzania, United Republic of' WHERE country = 'Tanzania';
+UPDATE sjr_import SET country = 'Moldova, Republic of' WHERE country = 'Moldova';
+UPDATE sjr_import SET country = 'Lao People''s Democratic Republic' WHERE country = 'Lao PDR';
+UPDATE sjr_import SET country = 'Bosnia and Herzegovina' WHERE country = 'Bosnia';
+UPDATE sjr_import SET country = 'Virgin Islands, U.s.' WHERE country = 'Virgin Islands (U.S.)';
+UPDATE sjr_import SET country = 'Trinidad and Tobago' WHERE country = 'Trinidad And Tobago';
+UPDATE sjr_import SET country = 'Russian Federation' WHERE country = 'Russia';
+UPDATE sjr_import SET country = 'Iran, Islamic Republic of' WHERE country = 'Iran';
+UPDATE sjr_import SET country = 'Viet Nam' WHERE country = 'Vietnam';
+UPDATE sjr_import SET country = 'Kyrgyzstan' WHERE country = 'Kyrgyz Republic';
+UPDATE sjr_import SET country = 'Saint Vincent and the Grenadines' WHERE country = 'St. Vincent And The Grenadines';
+UPDATE sjr_import SET country = 'Slovakia' WHERE country = 'Slovak Republic';
+UPDATE sjr_import SET country = 'Palestinian Territory, Occupied' WHERE country = 'West Bank And Gaza';
+UPDATE sjr_import SET country = 'Korea, Republic of' WHERE country = 'South Korea';
+UPDATE sjr_import SET country = 'Congo' WHERE country = 'Congo, Dem. Rep.';
+
+CREATE TABLE SJR (
+	country_id int ,
+    year integer NOT NULL,
+    value numeric
+);
+
+
+ALTER TABLE SJR
+  ADD PRIMARY KEY (country_id, year);
+
+ALTER TABLE SJR
+  ADD FOREIGN KEY (country_id) REFERENCES countries(id);
+  drop table if exists sjr_import;
--- main.py
+++ main.py
@@ -1,33 +1,25 @@
-from cmath import sin
-
-import mplcursors as mplcursors
-import psycopg2
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+from  Database import conxion as cx
 
-connection = psycopg2.connect(database="TER",
-                              user="postgres",
-                              password="halliche",
-                              host="localhost",
-                          port=5432)
-""""
-cursor = connection.cursor()
-cursor.execute("select * from countries")
-row=cursor.fetchall()
-for r in row:
-    print(r)
-cursor.close()
-"""
 test=pd.read_sql_query('''select avg(h.count)::numeric(10,2) as moyenne  ,h.year
 from homicides h
 group by h.year
-order by h.year''',connection)
+order by h.year''',cx.cnx)
 test1=pd.read_sql_query('''
 select sum(personal) as personal,year
 from gmi 
 group by year 
 order by year 
-''',connection)
+''',cx.cnx)
+
+df_in = pd.read_csv('sansjob.csv',error_bad_lines=False,delimiter=";")
+#df_out=df_in.set_index('Country Name').stack().reset_index()
+df_out = df_in.set_index(['Country Name']).stack().reset_index()
+df_out.columns = ["Country Name",'year','value']
+df_out.to_csv('sans_job_import.csv')
+
+
 fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,10))
 ax1.plot(test1['year'],test1['personal'])
 ax1.set_title("l'évolution de personal depuis 1990")
@@ -37,31 +29,4 @@ ax2.set_title("l'évolution d'homicide depuis 2000")
 ax2.set_ylabel("la moyenne d'homicide par années")
 plt.show()
 
-'''
-test.plot(kind="bar",x="year",y="moyenne",color='red')
-
-plt.ylabel("la moyenne d'homicide par années")
-plt.title("l'évolution d'homicide depuis 2000")
-
-#test1.plot(kind="line",x="year",y="personal")
-plt.ylabel("nombres de personal")
-plt.title("l'évolution de personal depuis 1990")
-mplcursors.cursor(hover=True)
-
-plt.show()
-
-
-'''
-
-'''df = pd.DataFrame.from_dict(test, orient='index', dtype=None)'''
-'''df.head()'''
-
-'''
-cursor = connection.cursor()
-cursor.execute("select  count(year),year from gmi group by year;")
-row=cursor.fetchall()
-for r in row:
-    print(test)
-cursor.close()
-'''
-connection.close()
+cx.cnx.close()
