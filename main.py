bcc46c5346bf06a71c9866976276ac57fef3fb41
Nadjibian <45125991+Nadjibian@users.noreply.github.com>
2021-04-23 15:37:14 +0200

    lier la carte du monde avec les graphiques en cliquant sur chaque pays

--- main.py
+++ main.py
@@ -1,70 +1,122 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-from plotly.validators.choropleth import _locationmode
+import multiprocessing
+import threading
 
-from  Database import conxion as cx
-
-test=pd.read_sql_query('''select avg(h.count)::numeric(10,2) as moyenne  ,h.year
-from homicides h
-group by h.year
-order by h.year''',cx.cnx)
-test1=pd.read_sql_query('''
-select sum(personal) as personal,year
-from gmi 
-group by year 
-order by year 
-''',cx.cnx)
-test2=pd.read_sql_query('''select nicename,year,value as value from bsa,countries where country=nicename ''',cx.cnx)
-'''
-df_in = pd.read_csv('sansjob.csv',error_bad_lines=False,delimiter=";")
-#df_out=df_in.set_index('Country Name').stack().reset_index()
-df_out = df_in.set_index(['Country Name']).stack().reset_index()
-df_out.columns = ["Country Name",'year','value']
-df_out.to_csv('sans_job_import.csv')
-'''
-'''
-fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,10))
-ax1.plot(test1['year'],test1['personal'])
-ax1.set_title("l'évolution de personal depuis 1990")
-ax1.set_ylabel("nombres de personal")
-ax2.bar(test['year'],test['moyenne'],color='red')
-ax2.set_title("l'évolution d'homicide depuis 2000")
-ax2.set_ylabel("la moyenne d'homicide par années")
-plt.show()
-cx.cnx.close()
-'''
-import json
-
-with open('pays.json') as json_file:
-    data = json.load(json_file)
-
+import pandas as pd     #(version 1.0.0)
+import plotly           #(version 4.5.0)
 import plotly.express as px
+from  Database import conxion as cx
+import matplotlib.pyplot as plt
+import dash             #(version 1.8.0)
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+# print(px.data.gapminder()[:15])
 
-fig = px.choropleth(data_frame = test2,geojson=data, locations='nicename',color='value',locationmode='country names',
-                    color_continuous_scale=px.colors.sequential.Plasma,
-                    title='Regions with Positive Cases',animation_frame='year',
-                    #labels={'value':'value bsa'}
-                          )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-config = dict({'scrollZoom': True})
-fig.show(config=config)
 
-'''
-fig = px.scatter_geo(data_frame=test2,
-    locations ="nicename",
-locationmode='country names',
-    color="value",
-    # what is the size of the biggest scatter point
-    size_max = 500,
-    projection="natural earth",
-    # range, important to keep the same range on all charts
-    range_color=(0,1000),
-    # columns which is in bold in the pop up
-    hover_name = "nicename",
-    # format of the popup not to display these columns' data
-    title="International Tourism",
-    animation_frame="year"
-                     )
-fig.update_geos(showcountries = True)
-fig.show()
-'''
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+it=pd.read_sql_query('''select nicename from countries''',cx.cnx)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+
+
+#---------------------------------------------------------------
+app.layout = html.Div([
+
+    html.Div([
+        dcc.Graph(id='the_graph')
+    ]),
+
+    html.Div([
+        dcc.Dropdown(
+            options=[{'label':i,'value':i} for i in it.nicename],
+            multi=True,
+            value="nicename",
+            id="input_state"
+        ),
+        html.Button(id='submit_button', n_clicks=0, children='Submit'),
+        html.Div(id='output_state'),
+    ],style={'text-align': 'center'}),
+
+])
+
+#---------------------------------------------------------------
+@app.callback(
+    [Output('output_state', 'children'),
+    Output(component_id='the_graph', component_property='figure')],
+    [Input(component_id='submit_button', component_property='n_clicks')],
+    [State(component_id='input_state', component_property='value')]
+)
+
+def update_output(num_clicks, val_selected):
+    if val_selected is None:
+        raise PreventUpdate
+    else:
+        df = pd.read_sql_query('''select nicename,year,value as value from bsa,countries where country=nicename ''',
+                                 cx.cnx)
+        fig = px.choropleth(df, locations='nicename', color='value',
+                            locationmode='country names',
+                            color_continuous_scale=px.colors.sequential.Plasma,
+                            title='Regions with Positive Cases', animation_frame='year',
+                            )
+
+        fig.update_layout(title=dict(font=dict(size=28),x=0.5,xanchor='center'),
+                          margin=dict(l=60, r=60, t=50, b=50))
+        if num_clicks>=1:
+            inche(val_selected)
+
+        return ('Le pays est : "{}" '.format(val_selected), fig)
+
+
+def inche(str):
+    cursor = cx.cnx.cursor()
+    t = ("SELECT id FROM countries WHERE nicename= %s "
+            )
+
+    # Execute cursor.
+    test1 =('''
+    select personal,year
+    from gmi g,countries s
+    where g.country_id=s.id and s.nicename=%s
+    ''')
+    df1 = pd.read_sql_query(test1, cx.cnx, params=str)
+
+    test5 = ('''SELECT P.value::numeric(10,2) as value  , P.year as year
+    FROM countries C , countries_in_regions CIR , world_regions WR , pib P  where  
+    CIR.region_id = WR.id  AND CIR.country_id = C.id AND P.country_id = C.id and C.nicename=%s''')
+    df2 = pd.read_sql_query(test5, cx.cnx, params=str)
+
+    import tkinter as tk
+    from pandas import DataFrame
+    import matplotlib.pyplot as plt
+    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+    root = tk.Tk()
+
+    figure3 = plt.Figure(figsize=(5, 4), dpi=100)
+    ax3 = figure3.add_subplot(111)
+    ax3.bar(df1['year'], df1['personal'], color='red')
+    scatter3 = FigureCanvasTkAgg(figure3, root)
+    scatter3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    ax3.legend([''])
+    ax3.set_xlabel('valeur de personals')
+    ax3.set_title('ev de personal ')
+
+    figure4 = plt.Figure(figsize=(5, 4), dpi=100)
+    ax4 = figure4.add_subplot(111)
+    ax4.scatter(df2['year'], df2['value'], color='g')
+    scatter3 = FigureCanvasTkAgg(figure4, root)
+    scatter3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    ax3.legend(['PIB'])
+    ax4.set_xlabel('vlauer de pib')
+    ax4.set_title('ev de pib depuis 1990')
+    root.mainloop()
+
+
+
+if __name__ == '__main__':
+    plt.switch_backend('agg')
+    app.run_server(debug=True)
