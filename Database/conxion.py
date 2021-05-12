bb94ecc61cd96a603899c9e1d5e200513db118d3
GT17S <guenanetoufik0615@gmail.com>
2021-05-13 01:26:37 +0200

    Ecarter les fichiers inutiles

--- Database/conxion.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(database="TER",
-                              user="postgres",
-                              password="halliche",
-                              host="localhost",
-    port=5432)
