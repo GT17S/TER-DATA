f2aabf9363c387937813656d300c9a39dffcbf95
Nadjibian <45125991+Nadjibian@users.noreply.github.com>
2021-05-14 11:17:42 +0200

    ajout des requêtes pour le fichier excel

---
+++ main.py
@@ -0,0 +1,27 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from src.apps import bsa,compar,app
+
+
+app.app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div([
+        dcc.Link(' Cliquez ici pour la comparaison des différents données ', href='/src/apps/compar',style={"color": "red", "size":"30"}),
+    ], className="row"),
+    html.Div(id='page-content', children=[])
+])
+
+
+@app.app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/src/apps/compar':
+        return compar.layout
+    else:
+        return bsa.layout
+
+
+if __name__ == '__main__':
+    app.app.run_server(debug=False)
